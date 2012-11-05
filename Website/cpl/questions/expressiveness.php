ccb4f6f6a1571f9fd5daddc9a924e397a420b552
KommuSoft <vanonsem.willem@gmail.com>
2012-11-05 13:00:58 +0000

    Verder gewerkt aan site
    git-svn-id: file:///home/kommusoft/tmpgit/svncpl@32 3f48092c-57cf-4b77-94f4-3dec9eb1df5f

--- Website/cpl/questions/expressiveness.php
+++ Website/cpl/questions/expressiveness.php
@@ -18,8 +18,8 @@ class Expressiveness extends Question {
 	}
 	
 	public function answers(){
-		$lua = parent::luaSays("");
-		$cpp = parent::cppSays("");
+		$lua = parent::luaSays("Lua has a very interesting feature: all of it's method calls are basically implemented in libraries. Since version 4, no standard built-in functions are available anyomore.");
+		$cpp = parent::cppSays("That's not true. Take for instance a for loop: one can say it's a language construct, on the other hand one can see it as a higher-order function call where the programmer gives the for-method the initializer, comparison, iterator and the loop-statements. Furthermore print ");
 		$cSharp = parent::cSharpSays("");
 		return $lua.$cpp.$cSharp;
 	}
