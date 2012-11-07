8d6ab0e3031337544c13b7eadaa25edb9a32b757
Gonaz <jonas.vanthornhout@student.kuleuven.be>
2012-11-07 21:51:37 +0000

    Het stukje over typing uitgeschreven. Een lijst van todo'tjes voor de website gemaakt (bedoeling is dat ik ze dit weekend zal oplossen en de mate dat ze een nuttige bijdrage leveren en tof zijn :p)
    git-svn-id: file:///home/kommusoft/tmpgit/svncpl@46 3f48092c-57cf-4b77-94f4-3dec9eb1df5f

--- Website/cpl/questions/typing.php
+++ Website/cpl/questions/typing.php
@@ -14,29 +14,61 @@ class Typing extends Question {
 	}
 	
 	public function introduction(){
-		return "Programmers agree on one thing: there is no agreement about what's the best typing system: static or dynamic [Jonas: strong, weak, safe, unsafe vermelden?]. What typing systems are used? Why? And more importanly: what are the advantages?";
+		return "Programmers agree on one thing: there is no agreement about what's the best typing system: static or dynamic, weak or strong. What typing systems are used? Why? And more importanly: what are the advantages?";
 	}
 	
 	public function answers(){
-		$pop_it = $this->popover("interferred typing","Interferred typing","Interferred typing is a language concept in C# where a programmer uses the keyword 'var' to denote the declaration of a variable. Instead of letting the programmer specify the type of the variable, the compiler will determine the type by intereference.");
-		$pop_tc = $this->popover("type conversions","Type Conversions","Type conversions are a language concepts in C# where a programmer can specify how an object of one type can be converted into another type. These conversions can be implicit. If they are implicit the programmer doesn't have to specify the object should convert itself.");
-		$lua = parent::luaSays("Lua has a dynamic type sytem. This reduces the code-compile-test cycle because the compile step can be reduced.");
+		$pop_it = $this->popover("interferred typing","Interferred typing","This is a language concept in C# where a programmer uses the keyword 'var' to denote the type declaration of a variable. Instead of letting the programmer specify the type of the variable, the compiler will determine the type by intereference. For C++ programmers: it's similar like 'auto'.");
 		
-		$cpp = parent::cppSays("But this way you can have problems at run-time while they could be detected at compile-time. So the programmer has the burden that he/she must write code that will be valid at run-time. Also for compiled languages it's cheaper to do compile-time type checking" . parent::bib()->cite(2));
+		$codeCoercion = 'a = 5
+b = "5"
+c = a+b
+print(c) -- output: 10
+print(a==b) -- output: false';
 		
-		$cSharp = parent::cSharpSays("It's true that static typing makes a programming language faster. The type system in C# is however more compilcated. I think even C++ won't have a problem with ".$pop_it." since the performance boost is equivalent. A second aspect are the ".$pop_tc.": a programmer specifies the conversions and the compiler will search for a chain of conversions to convert a variable to a given object. This is of course an argument to say C# is a weakly typed language. However since the program has to specify these conversions first, it's debatable.");
+		$codeRTTI = "x = {1, 2, 3}
+print(type(x)) -- output: table
+print(type(x[1])) -- output: number";
 		
-		$lua2 = parent::luaSays("Lua is also weakly typed. Lua uses a system that is named coercion. This system can convert numbers to strings and vice versa when this is appropriate." . parent::bib()->cite(4) . " An important exception is that coercion isn't performed in comparison operators. Furthermore the dynamic aspect enables other features in the language like functions. Functions are treated in Lua as variables. If you want a higher order programming language, it's somehow difficult to do this by static typing.");
+		//--------------------------
+		//Static: type that is inferred at compile-time
+		//Dynamic: type that is inferred at run-time
+		//Weak: automatic conversion
+		//Strong: no automatic conversion
+		//Weaker: int -> string but not string -> int
+		//Duck: [uitleg] + most of the duck type definitions use duck typing in a dynamic environment. We eliminate this constraint because some constructs that use types are run at compile time.
 		
-		$python = parent::pythonSays("Although weakly typed can be easy to use, it can also introduce ambiguity in the language. This makes is harder for beginners.");
+		$lua0 = parent::luaSays("Lua has a dynamic type sytem. One of the advantages is that a dynamic type system reduces the code-compile-test cycle because the compile step is shorter. This is because the type is infered at run-time which is less noticeable and can be done in a more lazy way. Another advantage is that a dynamic type system is simpler than a static system" . parent::bib()->cite(0) . ".");
 		
-		$lua3 = parent::luaSays("I agree. In retrospect, we could have removed this" . parent::bib()->cite(0) . ".");
+		$cpp0 = parent::cppSays("But don't you introduce an extra burden for the programmer? Some problems will only be detected at run-time while they could be prevented because the compiler would have spotted them. Also for compiled languages it's cheaper to do compile-time type checking" . parent::bib()->cite(2) . ".");
+		
+		$cSharp0 = parent::cSharpSays("It's true that static typing makes a programming language faster. However some programming languages have very complicated type systems, C# for example has " . $pop_it . ". This increases the work that the compiler must do, but it can result in better code because the most specific type will be used. Although this can look like weakly typed, it's actually strongly typed because the compiler will replace it to the correct type before compilation something that a programmer will not always do.");
+		
+		$lua1 = parent::luaSays("But doesn't a strong type system lead to horrible code? In strong type systems not every type is compatible with the expected type, so a lot of conversions are needed. Also the reuse of variables is more difficult.");
+		
+		$cpp1 = parent::cppSays("That's not completely true. You can use implicit casting. This means that the programmer specifies how an object of one type can be converted in another type. Then the compiler tries to cast the object in the desired type. Off course this is sometimes a source of unwanted behaviour. That's why a programmer can declare the cast 'explicit', that way you have to specify which cast you want to use where. I think C# has a similar thing?");
+		
+		$cSharp1 = parent::cSharpSays("Indeed, although it's called a conversion in C#. The programmer specifies the conversions and the compiler will search for a chain of conversions to convert a variable to a given object. This is of course an argument to say C# is a weakly typed language. However since the program has to specify these conversions first, it's debatable. It can make the type system less strong but it doesn't create a weak type system.");
+		
+		$lua2 = parent::luaSays("On the other hand, Lua is weakly typed. Lua uses a system that is named coercion. This system can convert numbers to strings and vice versa when this is appropriate" . parent::bib()->cite(4) . ". An important exception is that coercion isn't performed in comparison operators. The following example illustrates this." . parent::codeInline($codeCoercion, "lua") . "Furthermore the dynamic aspect enables other features in the language like functions. Functions are treated in Lua as variables. If you want a higher order programming language, it's somehow difficult to do this by static typing.");
+		
+		$python0 = parent::pythonSays("Although weakly typed can be easy to use, it can also introduce ambiguity in the language. This makes is harder for beginners. A beginner doesn't always understand that a program does what you write, not wat you want.");
+		
+		$lua3 = parent::luaSays("I agree. In retrospect, we could have removed this" . parent::bib()->cite(0) . ". The automatic conversion from strings to numbers can indeed be troublesome. But the automatic conversion from numbers to strings doesn't really lead to problems.");
+		
+		$cSharp2 = parent::cSharpSays("I want to react on argument of higher-order programming: it's not that difficult to implement it in the static typing paradigm: C# is a higher-order programming language. Methods can be stored as variables. These methods are then stored in a ''delegate'' structure. The delegate structure specifies the signature of the function. The compiler uses duck typing to check if the selected function matches with the given signature.");
+		
+		$lua4 = parent::luaSays("I know C# has some elegant structures to deal with higher-order programming. But the Lua support is even more elegant: use first-class values. However Lua is still a interpreted scripting language. That means code is compiled and checked at runtime. In order to make the run-time compiler fast, we needs small and simple code. C# is way too complex to be a fast scripting language. You also mentioned duck typing, this system is also used by Lua.");		
 
-		$cSharp2 = parent::cSharpSays("I want to react on argument of higher-order programming: it's not difficult to implement in the static typing paradigm: C# is a higer-order programming language. Methods can be stored as variables. These methods are then stored in a ''delegate'' structure. The delegate structure specifies the signature of the function. The compiler uses duck-typing to check if the selected function matches with the given signature.");
-		
-		$lua4 = parent::luaSays("I know C# has some elegant structures to deal with higher-order programming. However Lua is still a scripting language. That means code is compiled and checked at runtime. In order to make the runtime compiler fast, we needs small and simple code. C# is way too complex to be a fast scripting language.");		
+		$cpp2 = parent::cppSays("Something different. Lua is an embeddable language, this means that Lua should be compatible with the type system of the host language. How does Lua do this?");
 
-		return $lua.$cpp.$cSharp.$lua2.$python.$lua3.$cSharp2.$lua4;
+		$lua5 = parent::luaSays("Lua solves this problem with an abstract stack. The slots in this stack can contain Lua values with an arbitrary type. When a certain type has a mapping between C and Lua (for example numbers) there are 2 available functions. The injection function transforms a C value to a Lua one, the projection function works the other way around. The real problem is when there is no direct mapping. Then the API offers you to manipulate the stack positions.");
+		
+		$java0 = parent::javaSays("If a programmer doesn't have to specify the types of his/her variables in Lua, how can you check for the type?");
+		
+		$lua6 = parent::luaSays("It's not so that Lua doesn't use types internally, they are only transparent for the programmer. But to answer your question: Lua has support for run-time type information. For example:" . parent::codeInline($codeRTTI, "lua") . "So if a programmer would like to use the type of his/her variables, Lua provides an answer.");
+		
+		return $lua0.$cpp0.$cSharp0.$lua1.$cpp1.$cSharp1.$lua2.$python0.$lua3.$cSharp2.$lua4.$cpp2.$lua5.$java0.$lua6;
 	}
 	
 	public function link(){
