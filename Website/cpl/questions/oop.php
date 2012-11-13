2ba1bed1edb3494607ca2d662835ce9f356d2873
pjvuylsteke <pjvuylsteke@hotmai.com>
2012-11-13 15:03:20 +0000

    min schaamte overwonnen om mijn (misschien te simpele) dingen op te schrijven ivm oo
    git-svn-id: file:///home/kommusoft/tmpgit/svncpl@73 3f48092c-57cf-4b77-94f4-3dec9eb1df5f

--- Website/cpl/questions/oop.php
+++ Website/cpl/questions/oop.php
@@ -20,6 +20,40 @@ class OOP extends Question {
 	public function answers(){
 		$pop_events = $this->popover("events","Events","Events are a language structure in C# and is used as a syntactical sugar for the Visitor pattern. An event is basically a table of delegates (method pointers). By adding and removing, the programmer can register delegates to a certain event. The programmer can than call the event with the proper arguments resulting in a multicast of these arguments to the registered methods.");
 		$pop_operators = $this->popover("operators","Operators","C# supports operator overloading. Therefore one can define a + operator on a custom class (for instance complex numbers).");
+
+		$codeEx0Java = 
+"public class Car{
+	//attributes
+	private int speed;
+	
+	//constructor
+	public Car(){
+		this.speed = 0;
+	}
+
+	//other methods
+	public void accelerate(int speedIncrement){
+		this.speed += speedIncrement;
+	}
+}";
+
+		$codeEx0Lua = 
+"--attribute
+Car = {speed = 0}
+
+--constructor
+function Car.new(self,o)
+	o = o or {}
+	setmetatable(o,self)
+	self.__index = self
+	return o
+end
+
+--other methods
+function Car.accelerate(self,speedIncrement)
+	self.speed = self.speed+speedIncrement
+end
+";
 		
 		$codeEx1 = "int main(){
 	std::vector<int> v = {1,2,3,4,5,6};
@@ -65,6 +99,25 @@ print(fCar.speed) --The output is 190, because the implementation of the method 
 ";
 
 //er moet nog wat extra uitleg komen over metamethods.
+//metamethods.
+//operator overloading (ad hoc polymorphism) -> java niet; lua, c++, c#, python wel; haskell nog uitgebreider 
+//JavaScript is perhaps the best known prototype-based programming language, which employs cloning from prototypes rather than inheriting from a class. Another scripting language that takes this approach is Lua.
+//Prototype-based programming is a style of object-oriented programming in which classes are not present, and behavior reuse (known as inheritance in class-based languages) is performed via a process of cloning existing objects that serve as prototypes. This model can also be known as classless, prototype-oriented or instance-based programming. Delegation is the language feature that supports prototype-based programming.
+
+		$lua0 = parent::luaSays("The authors of Lua didn't want to turn Lua into an object-oriented language, because they didn't want to fix a programming paradigm for it. However, the mechanism of classes and objects can be implemented with tables. A table is the main, and the only, data structure in Lua. Tables are implemented as associative arrays. That are arrays where not only numbers can be used as indexes, but any other type (except nil). The other 7 types are number, string, table, function, userdata, boolean and thread. In that way, the keys of the table can be used as the names of the fields and methods by using the type string. Above that, functions are first-class values. This means that functions can be assigned to global and local variables, passed as arguments to functions, returned from functions and stored in tables. So, functions can be stored as the values in a table.");
+		$java0 = parent::javaSays("Java is an object-oriented language and there is direct support for classes. To give an example of a class:".$this->codeInline($codeEx0Java, "java")."Is it possible to give an analogue of this in Lua?");
+		$lua1 = parent::luaSays("Of course.".$this->codeInline($codeEx0Lua, "lua"). "I will clarify this piece of code a bit. Car is of type table as mentioned above. At line 2, an element with key \"speed\" and value 0 is added to the table. Line 2 is syntactic sugar for Car[\"speed\"] = 0. At line 5, we declare a function and put it in the table Car as well. Line 5 is also syntactic sugar for Car[\"new\"] = function (self,o).");
+		$cpp0 = parent::cppSays("For me, the body of the constructor isn't clear yet. What is meant with the setmetatable method and the \"__index\" construct?");
+		$lua2 = parent::luaSays("Before I explain this, I will give some background about metatables and metamethods. Metamethods allow changing the behaviour of a value when confronted with an undefined operation. So, when you try to access an absent field in a table, the result is nil. However, there is a way to get around this. When there is an absent field, the interpreter looks for an \"__index\" metamethod.");
+		//metamethods and tables uitleggen
+		//dan overgaan op inheritance
+		//eventueel multiple inheritance
+
+
+
+
+
+
 
 		$lua = parent::luaSays("Lua uses a quite special paradigm: the only real datastructure is a table. Lua itself contains only 8 types: nil, number, string, table, function, userdata, boolean and threads. Therefore one can argue Lua isn't a real object-oriented language. The authors of Lua are of course aware of the object-oriented paradigm and see it benefits. However in their opinion the language should remain simple and small. Lua however allows object-oriented programming since objects can be represented by the tables (where the keys are the names of the fields and methods). Inheritance in tables is supported by the so called ''fallback'' mechanism" . parent::bib()->cite(0) . ". This mechanism allows the programmer to propose a way to proceed if the basic execution mechanism fails. One can use this mechanism for instance when an index of a table doesn't exist by looking at the parent's table. Since inheritance is widely used one doesn't need to define these fallbacks manually. Lua offers an __index entry a pointer to the parent table. One can easily see the fallback mechanism gives birth to more dynamic typing: an object can change it's parent on the fly while keeping it's own specific attributes.".$this->codeInline($codeEx2, "lua"));
 		
@@ -74,7 +127,7 @@ print(fCar.speed) --The output is 190, because the implementation of the method 
 		
 		$python = parent::pythonSays("Pyhon is also an object oriented language. Actually the class mechanism is based on C++ and Modula-3. Python provides a very clean syntax for creating classes (and their hierarchies). Unfortunately constructors introduce uglier syntax (lots of underscores).");
 		
-		$lua2 = parent::luaSays("The main [Jonas: main is een gevaarlijk woord] issue with using a strict object-oriented paradigm is that it forces users to use objects everywhere. Not everything in the world can easily be modelled as objects. Also the usage of object orientation makes programming more difficult, because it forces you to think in a certain way.");
+		$lua2_old = parent::luaSays("The main [Jonas: main is een gevaarlijk woord] issue with using a strict object-oriented paradigm is that it forces users to use objects everywhere. Not everything in the world can easily be modelled as objects. Also the usage of object orientation makes programming more difficult, because it forces you to think in a certain way.");
 		
 		$cpp2 = parent::cppSays("C++ doesn't force you to use objects either. You can freely write some code outside a class, in fact a \"Main\" class isn't needed like some other languages. Also some functionality is available with nonmember functions and member functions. E.g." . $this->codeInline($codeEx1, "c++") . 'However this introduces "ambiguity" in the language.');
 		
@@ -82,7 +135,8 @@ print(fCar.speed) --The output is 190, because the implementation of the method 
 		
 		$code = $this->code($this->readFromFile("code/cppInheritance.cpp"), "c++");
 		
-		return $lua.$cpp.$cSharp.$python.$lua2.$cpp2.$java.$code;
+		return $lua0.$java0.$lua1.$cpp0.$lua2; 
+		//return $lua.$cpp.$cSharp.$python.$lua2_old.$cpp2.$java.$code;
 	}
 	
 	public function link(){
