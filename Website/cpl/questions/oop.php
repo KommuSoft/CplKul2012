1471086dd7d899d4232faa8f559320a681d86264
pjvuylsteke <pjvuylsteke@hotmai.com>
2012-11-19 12:43:07 +0000

    Enkele 'bugs' eruitgehaald
    git-svn-id: file:///home/kommusoft/tmpgit/svncpl@77 3f48092c-57cf-4b77-94f4-3dec9eb1df5f

--- Website/cpl/questions/oop.php
+++ Website/cpl/questions/oop.php
@@ -20,12 +20,13 @@ class OOP extends Question {
 	public function answers(){
 		$pop_events = $this->popover("events","Events","Events are a language structure in C# and is used as a syntactical sugar for the Visitor pattern. An event is basically a table of delegates (method pointers). By adding and removing, the programmer can register delegates to a certain event. The programmer can than call the event with the proper arguments resulting in a multicast of these arguments to the registered methods.");
 		$pop_operators = $this->popover("operators","Operators","C# supports operator overloading. Therefore one can define a + operator on a custom class (for instance complex numbers).");
+		$code = $this->code($this->readFromFile("code/cppInheritance.cpp"), "c++");
 
 		$code0 = $this->codeInline($this->readFromFile("code/JavaClassExample.java"), "java");
 		$code1 = $this->codeInline($this->readFromFile("code/LuaClassExample.lua"), "lua");
 		$code2 = $this->codeInline($this->readFromFile("code/LuaClassExampleAlternative.lua"), "lua");
 		$code3 = $this->codeInline($this->readFromFile("code/LuaClassExampleWithInheritance.lua"), "lua");
-		$code4 = $this->codeInline($this->readFromFile("code/Functionality.cpp"), "c++");
+		$code4 = $this->codeInline($this->readFromFile("code/AmbiguityExample.cpp"), "c++");
 
 //er moet nog wat extra uitleg komen over metamethods.
 //metamethods.
@@ -49,7 +50,7 @@ class OOP extends Question {
 
 
 
-		$lua = parent::luaSays("Lua uses a quite special paradigm: the only real datastructure is a table. Lua itself contains only 8 types: nil, number, string, table, function, userdata, boolean and threads. Therefore one can argue Lua isn't a real object-oriented language. The authors of Lua are of course aware of the object-oriented paradigm and see it benefits. However in their opinion the language should remain simple and small. Lua however allows object-oriented programming since objects can be represented by the tables (where the keys are the names of the fields and methods). Inheritance in tables is supported by the so called ''fallback'' mechanism" . parent::bib()->cite(0) . ". This mechanism allows the programmer to propose a way to proceed if the basic execution mechanism fails. One can use this mechanism for instance when an index of a table doesn't exist by looking at the parent's table. Since inheritance is widely used one doesn't need to define these fallbacks manually. Lua offers an __index entry a pointer to the parent table. One can easily see the fallback mechanism gives birth to more dynamic typing: an object can change it's parent on the fly while keeping it's own specific attributes.".$this->codeInline($codeEx2, "lua"));
+		$lua = parent::luaSays("Lua uses a quite special paradigm: the only real datastructure is a table. Lua itself contains only 8 types: nil, number, string, table, function, userdata, boolean and threads. Therefore one can argue Lua isn't a real object-oriented language. The authors of Lua are of course aware of the object-oriented paradigm and see it benefits. However in their opinion the language should remain simple and small. Lua however allows object-oriented programming since objects can be represented by the tables (where the keys are the names of the fields and methods). Inheritance in tables is supported by the so called ''fallback'' mechanism" . parent::bib()->cite(0) . ". This mechanism allows the programmer to propose a way to proceed if the basic execution mechanism fails. One can use this mechanism for instance when an index of a table doesn't exist by looking at the parent's table. Since inheritance is widely used one doesn't need to define these fallbacks manually. Lua offers an __index entry a pointer to the parent table. One can easily see the fallback mechanism gives birth to more dynamic typing: an object can change it's parent on the fly while keeping it's own specific attributes.");
 		
 		$cpp = parent::cppSays("C++ has good support for object oriented programming, in fact C++ is an object oriented language. For example, Strings are classes with their own member functions and such. Multiple inheritance is available"); //afgeleid van SIMULA
 		
@@ -59,11 +60,9 @@ class OOP extends Question {
 		
 		$lua2_old = parent::luaSays("The main [Jonas: main is een gevaarlijk woord] issue with using a strict object-oriented paradigm is that it forces users to use objects everywhere. Not everything in the world can easily be modelled as objects. Also the usage of object orientation makes programming more difficult, because it forces you to think in a certain way.");
 		
-		$cpp2 = parent::cppSays("C++ doesn't force you to use objects either. You can freely write some code outside a class, in fact a \"Main\" class isn't needed like some other languages. Also some functionality is available with nonmember functions and member functions. E.g." . $this->codeInline($codeEx1, "c++") . 'However this introduces "ambiguity" in the language.');
+		$cpp2 = parent::cppSays("C++ doesn't force you to use objects either. You can freely write some code outside a class, in fact a \"Main\" class isn't needed like some other languages. Also some functionality is available with nonmember functions and member functions. E.g." .$code4. 'However this introduces "ambiguity" in the language.');
 		
 		$java = parent::javaSays("Afgeleid van Smalltalk, multiple inheritance door interfaces");
-		
-		$code = $this->code($this->readFromFile("code/cppInheritance.cpp"), "c++");
 		
 		return $lua0.$java0.$lua1.$cpp0.$lua2.$java1; 
 		//return $lua.$cpp.$cSharp.$python.$lua2_old.$cpp2.$java.$code;
