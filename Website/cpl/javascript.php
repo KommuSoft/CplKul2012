05993ebee884fee5a6f465748c52849cf28004d7
Gonaz <jonas.vanthornhout@student.kuleuven.be>
2012-11-22 21:51:03 +0000

    Het syntax highlight probleempje zou opgelost moeten zijn. Zou iemand dat aub eens kunnen controleren?
    git-svn-id: file:///home/kommusoft/tmpgit/svncpl@129 3f48092c-57cf-4b77-94f4-3dec9eb1df5f

--- Website/cpl/javascript.php
+++ Website/cpl/javascript.php
@@ -2,18 +2,8 @@
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="js/jquery.js"></script>
-<script src="js/bootstrap-transition.js"></script>
-<script src="js/bootstrap-alert.js"></script>
-<script src="js/bootstrap-modal.js"></script>
-<script src="js/bootstrap-dropdown.js"></script>
-<script src="js/bootstrap-scrollspy.js"></script>
-<script src="js/bootstrap-tab.js"></script>
 <script src="js/bootstrap-tooltip.js"></script>
 <script src="js/bootstrap-popover.js"></script>
-<script src="js/bootstrap-button.js"></script>
-<script src="js/bootstrap-collapse.js"></script>
-<script src="js/bootstrap-carousel.js"></script>
-<script src="js/bootstrap-typeahead.js"></script>
 <script src="js/own.js"></script>
 <script src="js/jp-prettify.js"></script>
-<script src="js/prettify/lang-lua.js"></script>
+<script src="js/prettify/prettify.js"></script>
