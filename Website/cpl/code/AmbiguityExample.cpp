1d5e3668e501ebddebd481cd2382af8fc9e7628f
Gonaz <jonas.vanthornhout@student.kuleuven.be>
2012-11-23 08:45:52 +0000

    Code gecontroleerd (nutteloze bestanden verwijdered, c++ bestandje compileerbaar gemaakt)
    git-svn-id: file:///home/kommusoft/tmpgit/svncpl@142 3f48092c-57cf-4b77-94f4-3dec9eb1df5f

--- Website/cpl/code/AmbiguityExample.cpp
+++
@@ -1,5 +0,0 @@
-int main(){
-	std::vector<int> v = {1,2,3,4,5,6};
-	auto it0 = std::begin(v);
-	auto it1 = v.begin();
-}
--- Website/cpl/code/CPPMultipleAssignmentExample.cpp
+++ Website/cpl/code/CPPMultipleAssignmentExample.cpp
@@ -1,3 +1,9 @@
-int a,b;
-a,b = 5,10; // a is undefined, b is 5
-(a,b) = (5,10); //a is undefined, b is 10
+#include <iostream>
+
+int main(){
+	int a,b;
+	a,b = 5,10; // a is undefined, b is 5
+	std::cout << a << "\t" << b << std::endl;
+	(a,b) = (5,10); //a is undefined, b is 10
+	std::cout << a << "\t" << b << std::endl;
+}
