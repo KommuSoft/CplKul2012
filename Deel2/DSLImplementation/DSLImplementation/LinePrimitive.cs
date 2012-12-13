cb04239a855372b685366ef4527cad759efcbee1
KommuSoft <vanonsem.willem@gmail.com>
2012-12-13 15:28:42 +0000

    bugfix
    git-svn-id: file:///home/kommusoft/tmpgit/svncpl@149 3f48092c-57cf-4b77-94f4-3dec9eb1df5f

---
+++ Deel2/DSLImplementation/DSLImplementation/LinePrimitive.cs
@@ -0,0 +1,16 @@
+using System;
+using Cairo;
+
+namespace DSLImplementation {
+
+	public class LinePrimitive : IPaintPrimitive {
+
+		private PointD frm;
+		private PointD to;
+
+		public LinePrimitive () {
+		}
+
+	}
+}
+
