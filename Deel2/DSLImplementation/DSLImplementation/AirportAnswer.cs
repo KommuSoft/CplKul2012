a476760ae8545e4c441f0ed0dd91a06b103f19b3
pjvuylsteke <pjvuylsteke@hotmai.com>
2012-12-24 12:50:06 +0000

    een paar using dingen toegevoegd
    git-svn-id: file:///home/kommusoft/tmpgit/svncpl@166 3f48092c-57cf-4b77-94f4-3dec9eb1df5f

--- Deel2/DSLImplementation/DSLImplementation/AirportAnswer.cs
+++ Deel2/DSLImplementation/DSLImplementation/AirportAnswer.cs
@@ -1,17 +1,20 @@
 using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
 namespace DSLImplementation
 {
 	//The answer for the request of airports
 	[XmlRoot("AirportAnswer")]
 	public class AirportAnswer
 	{
-		public AirportAnswer ()
+		public AirportAnswer (List<Airport> Airports)
 		{
+			this.Airports = Airports;
 		}
 		
 		[XmlArray("ListOfAirports")]
 		[XmlArrayItem("Airport")]
-		public List<Airport> Airport{
+		public List<Airport> Airports{
 			get;
 			set;
 		}	
