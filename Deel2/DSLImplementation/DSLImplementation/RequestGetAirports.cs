83044a3dcaf3c12d14e9928636d9848408da0be8
Gonaz <jonas.vanthornhout@student.kuleuven.be>
2013-01-08 08:56:13 +0000

    De requestGetAirports zou nu moeten werken
    git-svn-id: file:///home/kommusoft/tmpgit/svncpl@247 3f48092c-57cf-4b77-94f4-3dec9eb1df5f

--- Deel2/DSLImplementation/DSLImplementation/RequestGetAirports.cs
+++ Deel2/DSLImplementation/DSLImplementation/RequestGetAirports.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Xml.Serialization;
+using System.Collections.Generic;
+
+
 namespace DSLImplementation.XmlRepresentation
 {
 	[XmlRoot("RequestGetAirports")]
@@ -26,19 +29,27 @@ namespace DSLImplementation.XmlRepresentation
 		public AnswerGetAirports execute()
 		{
 			Database.AirportRequest ar = new Database.AirportRequest();
+			List<Database.Airport> airports = new List<Database.Airport>();
 			if(this.Country != null && this.City != null){
-				List<Database.Airport> airports = ar.fetchAirportFromCityAndCountry(new Database.City(this.City.Name),new Database.Country(this.Country.Name));
+				airports = ar.fetchAirportFromCityAndCountry(new Database.City(this.City.Name),new Database.Country(this.Country.Name));
 			} else if(this.Country != null){
-				List<Database.Airport> airports = ar.fetchAirportFromCountry(new Database.Country(this.Country.Name));
+				airports = ar.fetchAirportFromCountry(new Database.Country(this.Country.Name));
 			} else if(this.City != null){
-				List<Database.Airport> airports = ar.fetchAirportFromCity(new Database.City(this.City.Name));
+				airports = ar.fetchAirportFromCityName(this.City.Name);
 			}
 			List<Airport> resultAirports = new List<Airport>();
 			foreach(Database.Airport a in airports){
-				resultAirports.Add(new Airport(City: this.City , Country: this.Country));
+				Database.CityRequest cir = new Database.CityRequest();
+				Database.CountryRequest cor = new Database.CountryRequest();
+				Database.City ci = cir.fetchFromID(a.city)[0];
+				Database.Country co = cor.fetchFromID(a.country)[0];
+
+				Country country = new Country(co.name);
+				//TODO airlines
+				resultAirports.Add(new Airport(Name: a.name, Code: a.code, City: new City(ci.name, country), Country: country, Airlines: new List<Airline>()));
 			}
 
-			return new AnswerGetCities(resultAirports);
+			return new AnswerGetAirports(resultAirports);
 		}		
 		
 	}
