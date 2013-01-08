8e46a46a58d6d1288b43fb2e8c31a46b126002a7
KommuSoft <vanonsem.willem@gmail.com>
2013-01-08 12:48:32 +0000

    code weer laten werken
    git-svn-id: file:///home/kommusoft/tmpgit/svncpl@274 3f48092c-57cf-4b77-94f4-3dec9eb1df5f

--- Deel2/DSLImplementation/DSLImplementation/TilePatterns1/RequestAddAirportTilePattern.cs
+++ Deel2/DSLImplementation/DSLImplementation/TilePatterns1/RequestAddAirportTilePattern.cs
@@ -1,12 +1,20 @@
 using System;
 
 namespace DSLImplementation.UserInterface {
-	
+		
 	public class RequestAddAirportTilePattern : TilePatternBase {
 
-		public RequestAddAirportTilePattern () {
+		public RequestAddAirportTilePattern () : base(null) {
 
 		}
+
+		#region implemented abstract members of DSLImplementation.UserInterface.TilePatternBase
+		protected override DSLImplementation.XmlRepresentation.IXmlRequest InternalToTransferCode (IPuzzlePiece root, System.Collections.Generic.Dictionary<string, object> bindings)
+		{
+			throw new System.NotImplementedException ();
+		}
+		#endregion
+
 
 	}
 
