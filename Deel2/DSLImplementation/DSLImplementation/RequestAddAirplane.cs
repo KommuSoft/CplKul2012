b2397fedd71de3b6edcb00cc3235c90e49db9a53
Gonaz <jonas.vanthornhout@student.kuleuven.be>
2013-01-08 10:22:20 +0000

    Request add airplane verbeterd
    git-svn-id: file:///home/kommusoft/tmpgit/svncpl@264 3f48092c-57cf-4b77-94f4-3dec9eb1df5f

--- Deel2/DSLImplementation/DSLImplementation/RequestAddAirplane.cs
+++ Deel2/DSLImplementation/DSLImplementation/RequestAddAirplane.cs
@@ -24,17 +24,26 @@ namespace DSLImplementation.XmlRepresentation
 		public override IXmlAnswer execute ()
 		{
 			List<int> seats = new List<int> ();
-			Database.SeatRequest sr = new Database.SeatRequest();
-			Database.ClassRequest cr = new Database.ClassRequest();
+			Database.SeatRequest sr = new Database.SeatRequest ();
+			Database.ClassRequest cr = new Database.ClassRequest ();
 
 			foreach (Seat s in this.Airplane.Seats) {
-				int classID = cr.fetchClassFromName (s.SeatClass.Name)[0].ID;
-				int seatID = sr.fetchSeatFromClassAndNumber(class_: classID, number: s.Number)[0].ID;
-				seats.Add(seatID);
+				int classID = cr.fetchClassFromName (s.SeatClass.Name) [0].ID;
+				int seatID = sr.fetchSeatFromClassAndNumber (class_: classID, number: s.Number) [0].ID;
+				seats.Add (seatID);
 			}
 
-			Database.Airplane airplane = new Database.Airplane(type: this.Airplane.Type, seat: seats);
-			throw new System.NotImplementedException ();
+			Database.Airplane airplane = new Database.Airplane (type: this.Airplane.Type, seat: seats);
+
+
+			AnswerAdd aa = new AnswerAdd ();
+			try {
+				airplane.insert();
+			} catch (Exception e) {
+				aa = new AnswerAdd(e.Message);
+			}
+
+			return aa;
 		}
 	}
 }
