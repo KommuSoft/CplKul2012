05596547d6ec177260b956f12868eb8e2b6695f9
Gonaz <jonas.vanthornhout@student.kuleuven.be>
2013-01-10 16:07:40 +0000

    Als het traject van de vlucht geen geldige locatie heeft dan wordt deze automatisch aangemaakt
    git-svn-id: file:///home/kommusoft/tmpgit/svncpl@331 3f48092c-57cf-4b77-94f4-3dec9eb1df5f

--- Deel2/DSLImplementation/DSLImplementation/Intermediate1/Flight.cs
+++ Deel2/DSLImplementation/DSLImplementation/Intermediate1/Flight.cs
@@ -2,6 +2,19 @@ using System;
 namespace DSLImplementation.IntermediateCode
 {
 	public class Flight{
+
+		public Flight (FlightTemplate Template, Airline Airline, DateTime StartDate, DateTime EndDate, Airport StartAirport, Airport DestinationAirport, Airplane Airplane, int distance)
+		{
+			this.distance = distance;
+			this.Template = Template;
+			this.Airline = Airline;
+			this.StartDate = StartDate;
+			this.EndDate = EndDate;
+			this.StartAirport = StartAirport;
+			this.DestinationAirport = DestinationAirport;
+			this.Airplane = Airplane;
+		}
+
 		public Flight (FlightTemplate Template, Airline Airline, DateTime StartDate, DateTime EndDate, Airport StartAirport, Airport DestinationAirport, Airplane Airplane){
 			this.Template = Template;
 			this.Airline = Airline;
@@ -12,7 +25,6 @@ namespace DSLImplementation.IntermediateCode
 			this.Airplane = Airplane;
 		}		
 
-		//TODO: de naam moet automatisch samengesteld worden
 		public String Name{ get; set;}
 		public Airline Airline{ get; set; }
 		public DateTime StartDate { get; set; }
@@ -22,6 +34,7 @@ namespace DSLImplementation.IntermediateCode
 		public Airport DestinationAirport { get; set; }
 		public Airplane Airplane { get; set; }
 		public FlightTemplate Template { get; set; }
+		public int distance { get; set; }
 	}
 }
 
--- Deel2/DSLImplementation/DSLImplementation/Intermediate1/RequestAddFlight.cs
+++ Deel2/DSLImplementation/DSLImplementation/Intermediate1/RequestAddFlight.cs
@@ -17,16 +17,31 @@ namespace DSLImplementation.IntermediateCode
 			Database.LocationRequest lr = new Database.LocationRequest ();
 			Database.AirlineRequest ar = new Database.AirlineRequest ();
 			Database.AirplaneRequest apr = new Database.AirplaneRequest ();
-			Database.SeatRequest sr = new Database.SeatRequest ();
-			Database.ClassRequest cr = new Database.ClassRequest ();
 			Database.FlightTemplateRequest ftr = new Database.FlightTemplateRequest ();
 
 			Console.WriteLine ("Info of airport 1 " + this.Flight.StartAirport.Code);
 
 			List<Database.Location> locations = lr.fetchLocationFromAirports (new Database.Airport (code: this.Flight.StartAirport.Code), new Database.Airport (code: this.Flight.DestinationAirport.Code));
 			if (locations.Count () == 0) {
-				//TODO dit moet automatisch de locatie aanmaken
-				return new AnswerAdd ("Couldn't find the location");
+				Database.AirportRequest airportRequest = new Database.AirportRequest();
+				List<Database.Airport> airports = airportRequest.fetchAirportFromCode(this.Flight.StartAirport.Code);
+				if(airports.Count() == 0){
+					return new AnswerAdd("Couldn't find the airport with code " + this.Flight.StartAirport.Code);
+				}
+				int airport1ID = airports[0].ID;
+
+				airports = airportRequest.fetchAirportFromCode(this.Flight.DestinationAirport.Code);
+				if(airports.Count() == 0){
+					return new AnswerAdd("Couldn't find the airport with code " + this.Flight.DestinationAirport.Code);
+				}
+				int airport2ID = airports[0].ID;
+
+				Database.Location location = new Database.Location(airport1ID, airport2ID, this.Flight.distance);
+				try{
+					location.insert();
+				} catch(Exception e){
+					return new AnswerAdd(e.Message);
+				}
 			}
 			int locationID = locations [0].ID;
 
@@ -35,12 +50,6 @@ namespace DSLImplementation.IntermediateCode
 				return new AnswerAdd ("Couldn't find the airline with code " + this.Flight.Airline.Code);
 			}
 			int airlineID = airlines [0].ID;
-
-//			List<int> seats = new List<int> ();
-//			foreach (Seat s in this.Flight.Airplane.Seats) {
-//				int classID = cr.fetchClassFromName (s.SeatClass.Name) [0].ID;
-//				seats.Add (sr.fetchSeatFromClassAndNumber (class_: classID, number: s.Number) [0].ID);
-//			}
 
 			List<Database.Airplane> airplanes = apr.fetchAirplaneFromCode (this.Flight.Airplane.Code);
 			if (airplanes.Count () == 0) {
@@ -54,12 +63,8 @@ namespace DSLImplementation.IntermediateCode
 			}
 			int templateID =  templates [0].ID;
 
-			Console.WriteLine("BLABLA " + templates[0].code);
-
 			Database.Flight flight = new Database.Flight (locationID, airlineID, this.Flight.StartDate, this.Flight.EndDate, airplaneID, templateID, this.Flight.TravelTime);
-
 			AnswerAdd aa = new AnswerAdd ();
-
 			try {
 				flight.insert();
 			} catch (Exception e) {
